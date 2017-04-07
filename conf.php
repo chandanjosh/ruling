a0244742d952e715564711b9ee6f5fcd4758602a
chandanjosh <chandanjoshi002@gmail.com>
2017-04-07 15:20:22 +0530

    Create conf.php

---
+++ conf.php
@@ -0,0 +1,5 @@
+<?php
+	define('SHOPIFY_SHOP', 'https://maketheme-2.myshopify.com/');
+	define('SHOPIFY_APP_API_KEY', '	357c22cf527898915171397037ff5249');
+	define('SHOPIFY_APP_PASSWORD', '71a08b3097549fec3f712b06211771d2');
+?>
